304a22a2748839995d7110c5cf1466ee54e34373
Oscar Nilestam <oscarnilestam@gmail.com>
2019-05-09 16:33:15 +0200

    Boilerplate Sinatra app

---
+++ app.rb
@@ -0,0 +1,12 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "pry-byebug"
+require "better_errors"
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+end
+
+get '/' do # <= Router part
+  'Who are you to wave your finger, you must have been out your head!!!'
+end
